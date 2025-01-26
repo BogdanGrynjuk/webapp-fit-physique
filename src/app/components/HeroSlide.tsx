0350b379563095e718baf6ca9ecf2927067a6964
BogdanGrynjuk <bgrynjuk@ukr.net>
2025-01-27 00:55:25 +0200

    add fadeIn animation  for elements HeroSlide

--- src/app/components/HeroSlide.tsx
+++ src/app/components/HeroSlide.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import React from 'react';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+
 import CustomButton from './CustomButton';
 
-import clsx from 'clsx';
+import { fadeIn } from '../lib/variants';
 
 const HeroSlide = ({
   title,
@@ -25,17 +28,36 @@ const HeroSlide = ({
       <div className="flex flex-col items-center lg:items-start lg:max-w-[700px]">
         <h1 className="h2 md:h1 flex flex-col gap-0 md:gap-2 lg:gap-4 text-center lg:text-left mb-2">
           <strong className="text-accent tracking-tighter">{title}</strong>
-          <span className=" text-white tracking-normal text-2xl md:text-4xl lg:text-7xl font-light ">
+          <motion.span
+            variants={fadeIn('up', 0.4)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}
+            className=" text-white tracking-normal text-2xl md:text-4xl lg:text-7xl font-light "
+          >
             {subtitle}
-          </span>
+          </motion.span>
         </h1>
-        <p className="font-roboto text-sm mobile:text-base tracking-wide text-white text-center lg:text-left mb-4">
+        <motion.p
+          variants={fadeIn('up', 0.6)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          className="font-roboto text-sm mobile:text-base tracking-wide text-white text-center lg:text-left mb-4"
+        >
           {description}
-        </p>
-        <CustomButton
-          text={buttonText}
-          containerStyles={'w-[146px] h-[40px] md:w-[162px] md:h-[56px]'}
-        />
+        </motion.p>
+        <motion.div
+          variants={fadeIn('up', 0.8)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <CustomButton
+            text={buttonText}
+            containerStyles={'w-[146px] h-[40px] md:w-[162px] md:h-[56px]'}
+          />
+        </motion.div>
       </div>
     </div>
   );
---
+++ src/app/lib/variants.tsx
@@ -0,0 +1,27 @@
+export const fadeIn = (direction: string, delay: number) => {
+  return {
+    show: {
+      opacity: 1,
+      y: 0,
+      x: 0,
+      transition: {
+        type: 'tween',
+        duration: 1.2,
+        delay: delay,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+
+    hidden: {
+      opacity: 0,
+      y: direction === 'up' ? 40 : direction === 'down' ? -40 : 0,
+      x: direction === 'left' ? 40 : direction === 'right' ? -40 : 0,
+      transition: {
+        type: 'tween',
+        duration: 1.4,
+        delay: delay,
+        ease: [0.25, 0.6, 0.3, 0.8],
+      },
+    },
+  };
+};
