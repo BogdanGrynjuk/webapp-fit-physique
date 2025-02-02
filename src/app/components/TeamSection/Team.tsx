7ca54d380256bfcb54d23ee6beab7196b04b139c
BogdanGrynjuk <bgrynjuk@ukr.net>
2025-02-02 02:15:04 +0200

    Add Team section

--- src/app/components/TeamSection/Team.tsx
+++ src/app/components/TeamSection/Team.tsx
@@ -1,12 +1,112 @@
+'use client';
+
 import React from 'react';
+import { FaFacebookF, FaXTwitter, FaYoutube } from 'react-icons/fa6';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../lib/variants';
+
+import TrainerCard from './TrainerCard';
+import CustomButton from '../UI/CustomButton';
+
+const trainers = [
+  {
+    photo: '/assets/img/trainers/david.jpg',
+    fullName: 'David Williams',
+    role: 'Body Builder Coach',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolore?',
+    onlineProfile: [
+      { icon: FaFacebookF, href: 'http://facebook.com' },
+      { icon: FaXTwitter, href: 'http://twitter.com' },
+      { icon: FaYoutube, href: 'http://youtube.com' },
+    ],
+  },
+  {
+    photo: '/assets/img/trainers/rosy.jpg',
+    fullName: 'Rosy Rivera',
+    role: 'Cardio Coach',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolore?',
+    onlineProfile: [
+      { icon: FaFacebookF, href: 'http://facebook.com' },
+      { icon: FaXTwitter, href: 'http://twitter.com' },
+      { icon: FaYoutube, href: 'http://youtube.com' },
+    ],
+  },
+  {
+    photo: '/assets/img/trainers/matt.jpg',
+    fullName: 'Matt Stonie',
+    role: 'Crossfit Coach',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolore?',
+    onlineProfile: [
+      { icon: FaFacebookF, href: 'http://facebook.com' },
+      { icon: FaXTwitter, href: 'http://twitter.com' },
+      { icon: FaYoutube, href: 'http://youtube.com' },
+    ],
+  },
+  {
+    photo: '/assets/img/trainers/sofia.jpg',
+    fullName: 'Sofia Lauren',
+    role: 'fitness Coach',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolore?',
+    onlineProfile: [
+      { icon: FaFacebookF, href: 'http://facebook.com' },
+      { icon: FaXTwitter, href: 'http://twitter.com' },
+      { icon: FaYoutube, href: 'http://youtube.com' },
+    ],
+  },
+];
 
 const Team = () => {
   return (
     <section
-      className="h-[calc(100vh-100px)] bg-purple-300 flex flex-col items-center justify-center"
+      className="pt-8 pb-8 lg:pt-14 lg:pb-14 min-h-[calc(100vh-100px)]"
       id="team"
     >
-      <h2 className="h2">Our Trainers</h2>
+      <div className="container mx-auto flex flex-col gap-10 items-center justify-between min-h-[calc(100vh-100px-(2*56px))]">
+        <motion.h2
+          variants={fadeIn('up', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          className="h2 text-center"
+        >
+          Our Trainers
+        </motion.h2>
+        <motion.ul
+          variants={fadeIn('up', 0.5)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5"
+        >
+          {trainers.map((trainer, index) => (
+            <li key={index}>
+              <TrainerCard
+                photo={trainer.photo}
+                fullName={trainer.fullName}
+                role={trainer.role}
+                description={trainer.description}
+                onlineProfile={trainer.onlineProfile}
+              />
+            </li>
+          ))}
+        </motion.ul>
+        <motion.div
+          variants={fadeIn('up', 0.6)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <CustomButton
+            containerStyles="w-[200px] h-[40px] md:w-[220px] md:h-[56px]"
+            text={'View All Trainers'}
+          />
+        </motion.div>
+      </div>
     </section>
   );
 };
--- src/app/components/TeamSection/TrainerCard.tsx
+++ src/app/components/TeamSection/TrainerCard.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+'use client';
 
-const TrainerCard = ({ fullName }) => {
-  return <></>;
+import Image from 'next/image';
+import React from 'react';
+import { IconType } from 'react-icons';
+import SocialLink from '../UI/SocialLink';
+
+type OnlineProfile = {
+  icon: IconType;
+  href: string;
+};
+
+type TrainerCardProps = {
+  photo: string;
+  fullName: string;
+  role: string;
+  description: string;
+  onlineProfile: OnlineProfile[];
+};
+
+const TrainerCard = ({
+  photo,
+  fullName,
+  role,
+  description,
+  onlineProfile,
+}: TrainerCardProps) => {
+  return (
+    <div className="text-center">
+      <div className="relative max-w-[280px] aspect-[280/314] mx-auto mb-4">
+        <Image
+          src={photo}
+          alt={`trainer ${fullName}`}
+          fill
+          className="object-cover"
+        />
+      </div>
+      <h4 className="h4 text-accent mb-2">{fullName}</h4>
+      <p className="font-roboto font-semibold text-primary-100/50 tracking-[2px] uppercase max-w-[300px] mx-auto mb-2">
+        {role}
+      </p>
+      <p className="font-roboto text-sm mobile:text-base max-w-[300px] mx-auto mb-4">
+        {description}
+      </p>
+      <ul className="flex items-center justify-center gap-5">
+        {onlineProfile.map((profile, index) => (
+          <li key={index}>
+            <SocialLink
+              href={profile.href}
+              Icon={profile.icon}
+              containerStyles="w-[44px] h-[44px]"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TrainerCard;
