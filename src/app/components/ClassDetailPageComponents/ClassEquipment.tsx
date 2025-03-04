92941c1d0aac2939ca7a3d7ccdfd134cd46222ed
BogdanGrynjuk <bgrynjuk@ukr.net>
2025-03-05 00:58:13 +0200

    Update ClassEquipment component

--- src/app/components/ClassDetailPageComponents/ClassEquipment.tsx
+++ src/app/components/ClassDetailPageComponents/ClassEquipment.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { ClassDetails } from '@/app/data/classes';
 
 type ClassEquipmentProps = {
-  equipment: ClassDetails;
+  equipment: Omit<ClassDetails, 'img'>;
 };
 const ClassEquipment = ({ equipment }: ClassEquipmentProps) => {
   const { title, text } = equipment;
   return (
-    <section>
-      <h2>{title}</h2>
-      <p>{text}</p>
+    <section className="w-full">
+      <div className="container mx-auto grid gap-4 xl:gap-8">
+        <h2 className="h2 text-center">{title}</h2>
+        <p className="text-sm mobile:text-base">{text}</p>
+      </div>
     </section>
   );
 };
