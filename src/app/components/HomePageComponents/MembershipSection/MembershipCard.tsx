d36165690247e72283ca2664d2920a953d4ee1f4
BogdanGrynjuk <bgrynjuk@ukr.net>
2025-03-16 00:42:07 +0200

    Update MembershipCard

--- src/app/components/HomePageComponents/MembershipSection/MembershipCard.tsx
+++ src/app/components/HomePageComponents/MembershipSection/MembershipCard.tsx
@@ -17,8 +17,8 @@ type MembershipCardProps = {
 const MembershipCard = ({ title, price, benefits }: MembershipCardProps) => {
   return (
     <div className="px-4 py-7">
-      <div className="border border-accent bg-primary-300/60 hover:bg-primary-300/90 hover:scale-105 transition-all duration-300 mx-auto max-w-[288px] md:max-w-sm xl:max-w-none">
-        <h4 className="h4 px-6 py-4 lg:px-4 xl:px-14 xl:py-5 border-b border-accent">
+      <div className="border border-accent bg-primary-300/60 hover:bg-primary-300/90 hover:scale-105 transition-all duration-300 mx-auto max-w-[288px] md:max-w-sm xl:max-w-none group">
+        <h4 className="h4 px-6 py-4 lg:px-4 xl:px-14 xl:py-5 border-b border-accent text-center group-hover:text-accent group-hover:tracking-[5px] transition-all duration-300">
           {title}
         </h4>
         <div className="flex flex-col items-center gap-8 px-6 py-4 lg:px-4 xl:px-14 xl:py-5">
@@ -40,12 +40,11 @@ const MembershipCard = ({ title, price, benefits }: MembershipCardProps) => {
             <strong className="text-6xl">{price}</strong>
             <em className="self-end text-2xl">/month</em>
           </p>
-          <div>
-            <CustomButton
-              text="buy now"
-              containerStyles="w-[146px] h-[40px] md:w-[162px] md:h-[56px]"
-            />
-          </div>
+
+          <CustomButton
+            text="buy now"
+            containerStyles="w-[146px] h-[40px] md:w-[162px] md:h-[56px]"
+          />
         </div>
       </div>
     </div>
--- src/app/components/UI/CustomButton.tsx
+++ src/app/components/UI/CustomButton.tsx
@@ -19,12 +19,11 @@ const CustomButton = ({
       type={type}
       className={clsx(
         containerStyles,
-        'group relative overflow-hidden bg-accent uppercase cursor-pointer',
+        'group/button relative overflow-hidden bg-accent uppercase cursor-pointer',
       )}
       onClick={onClick}
     >
-      <span className="absolute aspect-square bg-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45 scale-y-0 w-[200%] group-hover:scale-y-100 transition-all ease duration-300"></span>
-
+      <span className="absolute aspect-square bg-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45 scale-y-0 w-[200%] group-hover/button:scale-y-100 transition-all ease duration-300"></span>
       <span className="relative font-roboto text-white">{text}</span>
     </button>
   );
